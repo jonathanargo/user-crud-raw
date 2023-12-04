b0ee5aead1bba251590ef67b84dd8bba122a81a4
Jonathan Argo <jonathan.argo@gmail.com>
2023-12-04 16:46:58 -0500

    Separated layout

--- index.php
+++ index.php
@@ -7,83 +7,47 @@ use Models\User;
 
 $users = User::findAll();
 
+include 'layout/header.php';
 ?>
-<!DOCTYPE html>
-<html lang="en">
+    <main style="margin-top: 58px">
+        <div class="container pt-4">
+            <section class="mb-4">
+                <div class="card">
+                    <div class="card-header py-3">
+                        <h5 class="mb-0 text-left"><strong>Users</strong></h5>
+                    </div>
+                    <div class="card-body">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">First</th>
+                                    <th scope="col">Last</th>
+                                    <th scope="col">Email</th>
+                                    <th scope="col">Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php foreach ($users as $user) : ?>
+                                    <tr>
+                                        <th scope="row"><?= $user->id ?></th>
+                                        <td><?= $user->first_name ?></td>
+                                        <td><?= $user->last_name ?></td>
+                                        <td><?= $user->email ?></td>
+                                        <td>
+                                            <a href="view.php?id=<?= $user->id ?>" class="btn btn-secondary btn-sm">View</a>
+                                            <a href="edit.php?id=<?= $user->id ?>" class="btn btn-primary btn-sm">Edit</a>
+                                            <a href="delete.php?id=<?= $user->id ?>" class="btn btn-danger btn-sm">Delete</a>
+                                        </td>
+                                    </tr>
+                                <?php endforeach; ?>
+                            </tbody>
+                        </table>
 
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-  <meta http-equiv="x-ua-compatible" content="ie=edge" />
-  <title>Users</title>
-  <link
-    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
-    rel="stylesheet"
-    />
-    <!-- Google Fonts -->
-    <link
-    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-    rel="stylesheet"
-    />
-    <!-- MDB -->
-    <link
-    href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.1.0/mdb.min.css"
-    rel="stylesheet"
-    />
-</head>
-
-<body>
-  <!--Navigation goes here-->
-  <header>
-  </header>
-
-  <!--Main layout-->
-  <main style="margin-top: 58px">
-    <div class="container pt-4">
-      <section class="mb-4">
-        <div class="card">
-          <div class="card-header py-3">
-            <h5 class="mb-0 text-center"><strong>Users</strong></h5>
-          </div>
-          <div class="card-body">
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">Email</th>
-                        <th scope="col">Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                <?php foreach ($users as $user): ?>
-                    <tr>
-                        <th scope="row"><?= $user->id ?></th>
-                        <td><?= $user->first_name ?></td>
-                        <td><?= $user->last_name ?></td>
-                        <td><?= $user->email ?></td>
-                        <td>
-                            <a href="view.php?id=<?= $user->id ?>" class="btn btn-secondary btn-sm">View</a>
-                            <a href="edit.php?id=<?= $user->id ?>" class="btn btn-primary btn-sm">Edit</a>
-                            <a href="delete.php?id=<?= $user->id ?>" class="btn btn-danger btn-sm">Delete</a>
-                        </td>
-                    </tr>
-                <?php endforeach; ?>
-                </tbody>
-            </table>
-            
-          </div>
+                    </div>
+                </div>
+            </section>
         </div>
-      </section>
-    </div>
-  </main>
-  <!--Main layout-->
-  <!-- MDB -->
-  <script type="text/javascript" src="js/mdb.min.js"></script>
-  <!-- Custom scripts -->
-  <script type="text/javascript" src="js/admin.js"></script>
-
-</body>
-
-</html>
+    </main>
+<?php
+include 'layout/footer.php';
