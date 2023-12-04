32e64cd723c7ace3858876ea0fa8a4a5366949f5
Jonathan Argo <jonathan.argo@gmail.com>
2023-12-04 16:42:03 -0500

    Added MDB layout

--- helpers.php
+++ helpers.php
@@ -1,5 +1,7 @@
 <?php
 
+use Models\User;
+
 function dump($data)
 {
     echo '<pre>';
@@ -11,4 +13,24 @@ function dd($data)
 {
     dump($data);
     die();
+}
+
+function createUser() {
+    $user = new User();
+    $user->first_name = 'John';
+    $user->last_name = 'Doe';
+    $user->email = 'test@test.com';
+    $user->mobile_number = '1234567890';
+    $user->address = '123 Main St';
+    $user->city = 'New York';
+    $user->state = 'NY';
+    $user->zip = '12345';
+    $user->country = 'US';
+    $user->timezone = 'America/New_York';
+    $user->created = date('Y-m-d H:i:s');
+    $result = $user->save();
+    if (!$result) {
+        dd($user->getErrors());
+    }
+    dd($result);
 }
--- index.php
+++ index.php
@@ -3,42 +3,87 @@
 require 'vendor/autoload.php';
 require_once 'helpers.php';
 
-use Components\Database;
 use Models\User;
 
 $users = User::findAll();
 
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
+
 <head>
-    <title>Users</title>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta http-equiv="x-ua-compatible" content="ie=edge" />
+  <title>Users</title>
+  <link
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"
+    rel="stylesheet"
+    />
+    <!-- Google Fonts -->
+    <link
+    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+    rel="stylesheet"
+    />
+    <!-- MDB -->
+    <link
+    href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/7.1.0/mdb.min.css"
+    rel="stylesheet"
+    />
 </head>
+
 <body>
-    <h2>Users</h2>
-    <table>
-        <thead>
-            <tr>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Email Address</th>
-                <th>Actions</th>
-            </tr>   
-        </thead>
-        <tbody>
-            <?php foreach ($users as $user): ?>
-                <tr>
-                    <td><?= $user->first_name ?></td>
-                    <td><?= $user->last_name ?></td>
-                    <td><?= $user->email ?></td>
-                    <td>
-                        <a href="view.php?id=<?= $user->id ?>">View</a>
-                        <a href="edit.php?id=<?= $user->id ?>">Edit</a>
-                    </td>
-                </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
-    
+  <!--Navigation goes here-->
+  <header>
+  </header>
+
+  <!--Main layout-->
+  <main style="margin-top: 58px">
+    <div class="container pt-4">
+      <section class="mb-4">
+        <div class="card">
+          <div class="card-header py-3">
+            <h5 class="mb-0 text-center"><strong>Users</strong></h5>
+          </div>
+          <div class="card-body">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">First</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php foreach ($users as $user): ?>
+                    <tr>
+                        <th scope="row"><?= $user->id ?></th>
+                        <td><?= $user->first_name ?></td>
+                        <td><?= $user->last_name ?></td>
+                        <td><?= $user->email ?></td>
+                        <td>
+                            <a href="view.php?id=<?= $user->id ?>" class="btn btn-secondary btn-sm">View</a>
+                            <a href="edit.php?id=<?= $user->id ?>" class="btn btn-primary btn-sm">Edit</a>
+                            <a href="delete.php?id=<?= $user->id ?>" class="btn btn-danger btn-sm">Delete</a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+                </tbody>
+            </table>
+            
+          </div>
+        </div>
+      </section>
+    </div>
+  </main>
+  <!--Main layout-->
+  <!-- MDB -->
+  <script type="text/javascript" src="js/mdb.min.js"></script>
+  <!-- Custom scripts -->
+  <script type="text/javascript" src="js/admin.js"></script>
+
 </body>
+
 </html>
